72ffedc132f4542bbdb273b681e866c727519f95
filipenss <filipesupreme77@gmail.com>
2023-09-29 14:51:32 -0300

    commit

--- aula 14/script.js
+++ aula 14/script.js
@@ -1,22 +1,80 @@
+let input = document.querySelector("#cpf")
 
-//Mascara para cnpj
+//querySelector: tag, id, classse
 
-let inputCnpj=document.querySelector
-("#cnpj")
-inputCnpj.addEventListener("keypress", () ==>
+//para id e classe, utilizar a mesma notação usada pelo css
+
+ 
+
+// () função anônima
+
+// => arrow functon
+
+// funcion nome()
+
+input.addEventListener("keypress", () => {
+
+ 
+
+let inputlength = input.value.length
+
+ 
+
+    if (inputlength== 3 || inputlength== 7 ) {
+
+        input.value += "."
+
+    }
+
+ 
+
+    else if (inputlength== 11) {
+
+        input.value += "-"
+
+    }
+
+})
+
+ 
+
+///////////////////////////////////////////////////////////////
+
+//cnpj
+
+ 
+
+let inputCnpj=document.querySelector("#cnpj")
+
+inputCnpj.addEventListener("keypress" , () => {
+
+ 
+
+let cnpjlength = inputCnpj.value.length
+
+if (cnpjlength== 2 || cnpjlength== 6 )
+
 {
-    let cnpjLength = inputCnpj.value.length
-    if (cnpjLength==2 ||cnpjLength==6 )
-    {
-        inputCnpj.value+="."
-    }
 
-    if (cnpjLength==10)
-    {
-        inputCnpj.value+="/"
-    }
-    if (cnpjLength==15)
-    {
-        inputCnpj.value+="-"
-    }
+    inputCnpj.value+= "."
+
+}
+
+if (cnpjlength==10)
+
+{
+
+    inputCnpj.value+= "/"
+
+}
+
+if (cnpjlength==15)
+
+{
+
+    inputCnpj.value+= "-"
+
+}
+
+   
 })
